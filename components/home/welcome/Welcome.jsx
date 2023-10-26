06c10f0c6e6dbae0111ae2e14fc8b644ed8f87b9
JulianBristol <82460914+JulianBristol@users.noreply.github.com>
2023-10-26 17:13:47 -0400

    Add search bar

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import Reac, { useState } from 'react'
+import { 
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList
+} from 'react-native'
+import { useRouter } from 'expo-router';
 
 import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants'
 
 const Welcome = () => {
+
+  const router = useRouter();
+  const [input, setInput] = useState("");
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={ styles.userName }>Hello, Julian</Text>
+        <Text style={ styles.welcomeMessage }>Find your perfect job</Text>
+
+        <View style={ styles.searchContainer }>
+          <View style={ styles.searchWrapper}>
+            <TextInput
+            style={ styles.searchInput}
+            value={input}
+            onChange={(e) => {setInput(e.target.valueOf)}}
+            placeholder='What are you looking for?'
+            />
+          </View>
+        </View>
+      </View>
     </View>
   )
 }
